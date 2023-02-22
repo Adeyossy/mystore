0c01db212c9032be2f42dac669e0e88140550942
Adeyossy <adeyossy1@gmail.com>
2023-02-22 09:17:33 +0100

    feat: created product item components for list and grid views

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
+import { ProductListItemComponent } from './product-list-item/product-list-item.component';
+import { ProductGridItemComponent } from './product-grid-item/product-grid-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    ProductListItemComponent,
+    ProductGridItemComponent
   ],
   imports: [
     BrowserModule,
